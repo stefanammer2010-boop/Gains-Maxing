f820559fa67f4f3cc569886f60c9a9b65d6c2407
stefanammer2010-boop <stefan.ammer2010@gmail.com>
2026-08-20 10:48:14 +0200

    Delete src/supabase.js

--- src/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://kdwyvlbanmedjjqdmkcw.supabase.co";
-const supabaseAnonKey = "sb_publishable_PO3QVYtd-UtdzAnmeWSBmA_bCJWZbxJ";
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
